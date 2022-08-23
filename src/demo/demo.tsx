3fe197d931e674d740ddee907518819c625de685
Stephen Fraser <stephen.fraser@digirati.com>
2022-08-23 16:08:12 +0100

    Update demos

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -11,6 +11,7 @@ import { MediaControls } from './media-controls';
 import { ViewerControls } from './viewer-controls';
 import { useVisibleCanvases } from '../context/VisibleCanvasContext';
 import { useEffect, useState } from 'react';
+import { parse } from 'query-string';
 
 function Demo() {
   const manifest = useManifest();
@@ -54,18 +55,14 @@ function Demo() {
 const demo = document.getElementById('root')!;
 
 const App = () => {
-  const [id, setId] = useState<string>(
-    () =>
-      window.location.hash.slice(1).replace(/manifest=/, '') ||
-      'https://gist.githubusercontent.com/stephenwf/57cc5024144c53d48cc3c07cc522eb94/raw/a87a5d9a8f949bfb11cebd4f011a204abe8a932b/manifest.json'
+  const [queryString, setQueryString] = useState<{ manifest?: string; range?: string; canvas?: string }>(() =>
+    parse(window.location.hash.slice(1))
   );
+  const { manifest, range, canvas } = queryString;
 
   useEffect(() => {
     const hashChange = () => {
-      setId(
-        window.location.hash.slice(1).replace(/manifest=/, '') ||
-          'https://gist.githubusercontent.com/stephenwf/57cc5024144c53d48cc3c07cc522eb94/raw/a87a5d9a8f949bfb11cebd4f011a204abe8a932b/manifest.json'
-      );
+      setQueryString(parse(window.location.hash.slice(1)));
     };
     window.addEventListener('hashchange', hashChange);
 
@@ -74,7 +71,15 @@ const App = () => {
 
   return (
     <VaultProvider>
-      <SimpleViewerProvider pagingEnabled={true} startCanvas={' ... '} rangeId={' ... '} manifest={id}>
+      <SimpleViewerProvider
+        pagingEnabled={true}
+        startCanvas={canvas}
+        rangeId={range}
+        manifest={
+          manifest ||
+          'https://gist.githubusercontent.com/stephenwf/57cc5024144c53d48cc3c07cc522eb94/raw/a87a5d9a8f949bfb11cebd4f011a204abe8a932b/manifest.json'
+        }
+      >
         <Demo />
       </SimpleViewerProvider>
     </VaultProvider>
