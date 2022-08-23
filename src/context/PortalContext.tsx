cca3c6ce9766741f474aaf55db131996c763961a
Stephen Fraser <stephen.fraser@digirati.com>
2022-08-23 15:26:15 +0100

    Portal + Thumbnail fixes

--- src/context/PortalContext.tsx
+++ src/context/PortalContext.tsx
@@ -1,31 +1,21 @@
-import React, { ReactNode, useContext, useLayoutEffect, useMemo, useRef } from 'react';
-import { createRoot, Root } from 'react-dom/client';
+import React, { ReactNode, useContext, useLayoutEffect } from 'react';
+import { Root } from 'react-dom/client';
 import { useViewerPreset, ViewerPresetContext } from './ViewerPresetContext';
 
-export const PortalContext = React.createContext<HTMLDivElement | null>(null);
-export const OverlayPortalContext = React.createContext<HTMLDivElement | null>(null);
+export const PortalContext = React.createContext<Root | null>(null);
+export const OverlayPortalContext = React.createContext<Root | null>(null);
 
 export function CanvasPortal({ children, overlay }: { children: ReactNode; overlay?: boolean }) {
   const htmlElement = useContext(overlay ? OverlayPortalContext : PortalContext);
-  const rootRef = useRef<Root | null>(null);
   const preset = useViewerPreset();
 
   useLayoutEffect(() => {
-    if (!rootRef.current) {
-      rootRef.current = htmlElement ? createRoot(htmlElement) : null;
-    }
-  }, []);
-
-  useLayoutEffect(() => {
-    return () => {
-      rootRef.current?.unmount();
-      rootRef.current = null;
-    };
-  }, []);
-
-  useLayoutEffect(() => {
-    if (rootRef.current) {
-      rootRef.current.render(<ViewerPresetContext.Provider value={preset}>{children}</ViewerPresetContext.Provider>);
+    if (htmlElement) {
+      try {
+        htmlElement.render(<ViewerPresetContext.Provider value={preset}>{children}</ViewerPresetContext.Provider>);
+      } catch (e) {
+        // ignore..
+      }
     }
   }, [children, preset]);
 
