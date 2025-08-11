9de3b53c63afb78d0516818bb3ffbe381f6611a4
Stephen Fraser <stephen.fraser@digirati.com>
2025-08-11 19:05:35 +0100

    Expanded context menu with hook for state.

--- src/hooks/useAtlasContextMenu.tsx
+++ src/hooks/useAtlasContextMenu.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
+import { create } from 'zustand';
 import { RenderHighlightAnnotation } from '../canvas-panel/render/HighlightAnnotation';
 
 export type RenderContextProps = {
@@ -7,24 +8,54 @@ export type RenderContextProps = {
   close: () => void;
 };
 
+export const useContextMenuStore = create<{
+  isMenuOpen: boolean;
+  menuPosition: { x: number; y: number };
+  open(position: { x: number; y: number }): void;
+  toggle(position: { x: number; y: number }): void;
+  setMenuPosition(position: { x: number; y: number }): void;
+  setIsMenuOpen(isMenuOpen: boolean | ((prev: boolean) => boolean)): void;
+  close(): void;
+}>()((set) => ({
+  isMenuOpen: false,
+  menuPosition: { x: 0, y: 0 },
+  setMenuPosition(menuPosition: { x: number; y: number }) {
+    set({ menuPosition });
+  },
+  toggle(menuPosition?: { x: number; y: number }) {
+    set((state) => ({ isMenuOpen: !state.isMenuOpen, menuPosition }));
+  },
+  open(menuPosition: { x: number; y: number }) {
+    set({ isMenuOpen: true, menuPosition });
+  },
+  setIsMenuOpen(isMenuOpen) {
+    if (typeof isMenuOpen === 'function') {
+      set((state) => ({ isMenuOpen: isMenuOpen(state.isMenuOpen) }));
+    } else {
+      set({ isMenuOpen });
+    }
+  },
+  close() {
+    set({ isMenuOpen: false });
+  },
+}));
+
 export function useAtlasContextMenu(
   id: string,
   canvasId?: string,
   renderContextMenu?: (props: RenderContextProps) => React.ReactNode,
 ) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
+  const { isMenuOpen, setIsMenuOpen, close, open, menuPosition, setMenuPosition, toggle } = useContextMenuStore();
 
   const onContextMenu = useCallback(
     (e: any) => {
       if (renderContextMenu) {
-        setMenuPosition(e.atlas);
         e.preventDefault();
         e.stopPropagation();
-        setIsMenuOpen((o) => !o);
+        toggle(e.atlas);
       }
     },
-    [renderContextMenu],
+    [toggle, renderContextMenu],
   );
 
   const menu = (
@@ -46,5 +77,5 @@ export function useAtlasContextMenu(
 
   const props = { onContextMenu };
 
-  return [menu, props, { isMenuOpen, setIsMenuOpen, menuPosition, setMenuPosition }] as const;
+  return [menu, props, { open, close, toggle, isMenuOpen, setIsMenuOpen, menuPosition, setMenuPosition }] as const;
 }
