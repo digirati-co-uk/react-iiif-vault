40213b98a6799ff8211f48ab2559e84d2dbe325c
Stephen Fraser <stephen.fraser@digirati.com>
2025-08-19 16:21:37 +0100

    Fixes

--- src/utility/serialise-supported-selector.ts
+++ src/utility/serialise-supported-selector.ts
@@ -8,12 +8,21 @@ export function isBoxSelector(t: SupportedSelector): t is BoxSelector {
   return t.type === 'BoxSelector';
 }
 
-export function seraliseSupportedSelector(selector: SupportedSelector) {
+export function seraliseSupportedSelector(selector: SupportedSelector, on?: { width: number; height: number }) {
   if (isSvgSelector(selector)) {
     const notOpen = selector.svgShape === 'polyline';
 
     if (!selector.points || selector.points.length === 0) {
       return null;
+    }
+
+    if (on) {
+      const { width, height } = on;
+      const el = !notOpen ? 'polyline' : 'polygon';
+      return {
+        type: 'SvgSelector',
+        value: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}" width="${width}" height="${height}"><${el} points="${selector.points.map((p: any) => p.join(',')).join(' ')}" /></svg>`,
+      };
     }
 
     return {
