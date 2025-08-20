a32bf2e2eeec1327c78dc23efae6104ec3df5554
Stephen Fraser <stephen.fraser@digirati.com>
2025-08-20 14:45:04 +0100

    Updated use current anno metadata

--- src/hooks/useCurrentAnnotationMetadata.ts
+++ src/hooks/useCurrentAnnotationMetadata.ts
@@ -1,11 +1,20 @@
+import { useCallback } from 'react';
 import { useStore } from 'zustand';
 import { useAtlasStore } from '../canvas-panel/context/atlas-store-provider';
 
-export function useCurrentAnnotationMetadata() {
+export function useCurrentAnnotationMetadata({ requestId: customRequestId }: { requestId?: string } = {}) {
   const store = useAtlasStore();
   const metadata = useStore(store, (state) => state.metadata);
-  const requestId = useStore(store, (state) => state.tool.requestId);
-  const updateMetadata = useStore(store, (state) => state.setMetadata);
+  const currentRequestId = useStore(store, (state) => state.tool.requestId);
+  const requestId = customRequestId || currentRequestId;
+  const updateMetadataInternal = useStore(store, (state) => state.setMetadata);
+
+  const updateMetadata = useCallback(
+    (data: Record<string, any>) => {
+      return updateMetadataInternal(data, requestId || undefined);
+    },
+    [updateMetadataInternal, requestId],
+  );
 
   return [
     //
