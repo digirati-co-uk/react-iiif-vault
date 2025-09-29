004ccbd82194f817bee7bab6e1447de809ce2c92
Stephen Fraser <stephen.fraser@digirati.com>
2025-09-29 16:51:54 +0100

    reorder logic

--- src/hooks/useRemoteStylesheet.ts
+++ src/hooks/useRemoteStylesheet.ts
@@ -8,11 +8,14 @@ interface StylesheetStore {
   errors: Array<{ url: string; error: Error }>;
 
   // Actions.
-  loadStylesheet(url: string): Promise<void>;
+  loadStylesheet(url: string): Promise<string>;
   setStylesheet(value: string, url?: string): string;
+  parseStylesheet(
+    stylesheet: Annotation['stylesheet'] | null
+  ): Promise<null | { id: string; type: 'CssStylesheet'; value: string }>;
 }
 
-const useStylesheetStore = create<StylesheetStore>()((set, get) => {
+export const useStylesheetStore = create<StylesheetStore>()((set, get) => {
   const idMap = new Map<string, string>();
 
   const randomId = () => Math.random().toString(36).substring(2, 15);
@@ -29,18 +32,74 @@ const useStylesheetStore = create<StylesheetStore>()((set, get) => {
     loading: [],
     errors: [],
 
+    async parseStylesheet(stylesheet) {
+      const { loadStylesheet, setStylesheet } = get();
+
+      if (!stylesheet) {
+        return null;
+      }
+
+      if (typeof stylesheet === 'string') {
+        if (stylesheet.startsWith('http')) {
+          return {
+            id: stylesheet,
+            type: 'CssStylesheet',
+            value: await loadStylesheet(stylesheet),
+          };
+        } else {
+          const id = setStylesheet(stylesheet);
+          return {
+            id: id,
+            type: 'CssStylesheet',
+            value: stylesheet,
+          };
+        }
+      }
+
+      if (stylesheet.type === 'CssStylesheet') {
+        const id = (stylesheet as any).id;
+
+        if ('value' in stylesheet) {
+          // Inline stylesheet.
+          const value = Array.isArray(stylesheet.value) ? stylesheet.value.join('\n') : stylesheet.value;
+          if (value) {
+            const sheetId = setStylesheet(value, id);
+            return {
+              id: sheetId,
+              type: 'CssStylesheet',
+              value: value,
+            };
+          }
+        }
+
+        if ('id' in stylesheet) {
+          if (stylesheet.id) {
+            const sheet = await loadStylesheet(stylesheet.id);
+            return {
+              id: stylesheet.id,
+              type: 'CssStylesheet',
+              value: sheet,
+            };
+          }
+        }
+      }
+      return null;
+    },
+
     // Actions.
     async loadStylesheet(url: string) {
       const loading = get().loading;
-      if (loading.includes(url)) return;
+      if (loading.includes(url)) return '';
 
       try {
         const response = await fetch(url);
         if (!response.ok) throw new Error(`Failed to load stylesheet from ${url}`);
         const stylesheet = await response.text();
         set((current) => ({ stylesheets: { ...current.stylesheets, [url]: stylesheet } }));
+        return stylesheet;
       } catch (error: any) {
         set((current) => ({ errors: [...current.errors, { url, error }] }));
+        return '';
       }
     },
     setStylesheet(value: string, url?: string) {
@@ -53,44 +112,21 @@ const useStylesheetStore = create<StylesheetStore>()((set, get) => {
 
 export function useRemoteStylesheet(stylesheet?: Annotation['stylesheet'] | null) {
   const [id, setId] = useState<string>('');
-  const { loading, errors, stylesheets, loadStylesheet, setStylesheet } = useStylesheetStore();
+  const { loading, errors, stylesheets, parseStylesheet } = useStylesheetStore();
 
   useEffect(() => {
     if (!stylesheet) return;
 
-    if (typeof stylesheet === 'string') {
-      if (stylesheet.startsWith('http')) {
-        setId(stylesheet);
-        loadStylesheet(stylesheet);
-      } else {
-        setId(setStylesheet(stylesheet));
+    parseStylesheet(stylesheet).then((resp) => {
+      if (resp?.id) {
+        setId(resp.id);
       }
-      return;
-    }
-
-    if (stylesheet.type === 'CssStylesheet') {
-      const id = (stylesheet as any).id;
-
-      if ('value' in stylesheet) {
-        // Inline stylesheet.
-        const value = Array.isArray(stylesheet.value) ? stylesheet.value.join('\n') : stylesheet.value;
-        if (value) {
-          setId(setStylesheet(value, id));
-        }
-      }
-
-      if ('id' in stylesheet) {
-        if (stylesheet.id) {
-          loadStylesheet(stylesheet.id);
-        }
-        return;
-      }
-    }
-  }, [stylesheet, setStylesheet, loadStylesheet]);
+    });
+  }, [stylesheet, parseStylesheet]);
 
   const onlySheets = useMemo(() => {
     const sheets: Record<string, string> = {};
-    if (stylesheets[id]) {
+    if (id && stylesheets[id]) {
       sheets[id] = stylesheets[id];
     }
 
